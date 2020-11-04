f5c7cd40c7a32ff1ed441d71b0882586981d7159
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-11-04 16:39:51 +0200

    Update Udemy_bot.py

--- Udemy_bot.py
+++ Udemy_bot.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
-import requests,threading,glob
+import requests, threading,glob,sys
 import bs4 as bs
+
 
 def is_rate_valid(element):
     '''
@@ -11,11 +12,12 @@ def is_rate_valid(element):
     :return: if the current course meets the conditions
     '''
 
-    rate=element.find("div",{"class":"coupon-details-extra-3"}).find_all('p')[2].text
-    rate=rate.split('Rate: ')[1]
-    stars=float(rate.split('/')[0])
-    people=int(rate.split('/')[1])
-    return stars>=4.2 and people>=200
+    rate = element.find("div", {"class": "coupon-details-extra-3"}).find_all('p')[2].text
+    rate = rate.split('Rate: ')[1]
+    stars = float(rate.split('/')[0])
+    people = int(rate.split('/')[1])
+    return stars >= 4.2 and people >= 200
+
 
 def is_valid_coupon(element):
     '''
@@ -27,6 +29,7 @@ def is_valid_coupon(element):
     button = element.find("a", {"class": "button-icon"}).text
     return "Expired" not in button
 
+
 def get_udemy_link(element):
     '''
     Gets the relevent link to the udemy course
@@ -34,10 +37,11 @@ def get_udemy_link(element):
     :return: the relevent link to the udemy course
     '''
 
-    link1=element.find("a", {"class": "button-icon"})['href']
+    link1 = element.find("a", {"class": "button-icon"})['href']
     source = requests.get(link1)
     soup = bs.BeautifulSoup(source.content, 'lxml')
-    return soup.find("a",{"class":"button-icon"})['href']
+    return soup.find("a", {"class": "button-icon"})['href']
+
 
 def add_links(soup):
     '''
@@ -56,7 +60,7 @@ def find_potential_urls():
     Finds all relevent courses
     '''
     print("Looking for potential links")
-    for l in range(1, 217): #might needed pages update
+    for l in range(1, 217):  # might needed pages update
         url = "https://www.udemyfreebies.com/course-category/it-and-software/" + str(l)
         source = requests.get(url)
         soup = bs.BeautifulSoup(source.content, 'lxml')
@@ -65,7 +69,7 @@ def find_potential_urls():
         threading.Thread(target=add_links, args=(soup,)).start()
 
     # you can add more categories
-    for l in range(1, 305): #might needed pages update
+    for l in range(1, 305):  # might needed pages update
         url = "https://www.udemyfreebies.com/course-category/development/" + str(l)
         source = requests.get(url)
         soup = bs.BeautifulSoup(source.content, 'lxml')
@@ -83,20 +87,22 @@ def click():
     '''
 
     try:
-        browser.find_element_by_xpath("//div[contains(@class,'ud-component--course-landing-page-udlite--buy-button-cacheable')]").click()
+        browser.find_element_by_xpath(
+            "//div[contains(@class,'ud-component--course-landing-page-udlite--buy-button-cacheable')]").click()
         sleep(1)
     except:
         pass
     try:
         browser.find_element_by_class_name("slider-menu__buy-button").find_element_by_tag_name("button").click()
         sleep(2)
-        browser.find_element_by_class_name("styles--complete-payment-container--3Jazs").find_element_by_tag_name("button").click()
+        browser.find_element_by_class_name("styles--complete-payment-container--3Jazs").find_element_by_tag_name(
+            "button").click()
         sleep(1)
     except:
         pass
 
-def is_account_exist(email,password):
 
+def is_account_exist(email, password):
     '''
     Checks if the credentials are correct
     :param email: users email
@@ -106,8 +112,8 @@ def is_account_exist(email,password):
 
     print("Cheacking the email and the password are correct")
     options = Options()
-    #options.add_argument("--incognito")
-    #options.add_argument("--headless")
+    # options.add_argument("--incognito")
+    # options.add_argument("--headless")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     browser = webdriver.Chrome(options=options)
     browser.set_window_position(5000, 5000)
@@ -117,29 +123,30 @@ def is_account_exist(email,password):
     sleep(1)
     browser.find_element_by_id("id_password").send_keys(password)
     sleep(1)
-    temp_url=browser.current_url
+    temp_url = browser.current_url
     browser.find_element_by_id("submit-id-submit").click()
     sleep(4)
-    is_exist=temp_url==browser.current_url
+    is_exist = temp_url == browser.current_url
     browser.close()
     return is_exist
-    
+
+
 if __name__ == '__main__':
 
-        if len(sys.argv)!=3:
+    if len(sys.argv) != 3:
         print("Wrong input make sure you type you Email and Password")
         exit()
-    elif is_account_exist(sys.argv[1],sys.argv[2]):
+    elif is_account_exist(sys.argv[1], sys.argv[2]):
         print("There was a problem logging in.Check your email and password or create an account.")
         exit()
 
-    email=sys.argv[1]
-    password=sys.argv[2]
+    email = sys.argv[1]
+    password = sys.argv[2]
 
     potential_urls = []
     if glob.glob("urls.txt"):
         with open("urls.txt") as f:
-            potential_urls=f.read().split("\n")
+            potential_urls = f.read().split("\n")
     else:
         find_potential_urls()
         ###################
@@ -163,16 +170,20 @@ if __name__ == '__main__':
     browser.find_element_by_id("id_password").send_keys(password)
     sleep(1)
     browser.find_element_by_id("submit-id-submit").click()
-
+    
     print("saving the courses to your udemy account")
-    course_count=0
+    course_count = 0
     for url in potential_urls:
         try:
             browser.get(url)
             sleep(2)
-            click()
-            course_count+=1
+            if "Free" in browser.find_element_by_xpath \
+                        (
+                        "//div[contains(@class, 'price-text--price-part--Tu6MH udlite-clp-discount-price udlite-heading-lg')]") \
+                    .find_elements_by_xpath(".//span")[1].text:
+                click()
+                course_count += 1
         except:
             pass
-    print("Done added "+str(course_count)+" courses")
+    print("Done added " + str(course_count) + " courses")
     browser.close()
