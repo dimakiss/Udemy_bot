48a86140327fb8a183880a94ee576c476c969a12
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-11-16 17:36:01 +0200

    Update Udemy_bot.py
    Adding feature #7

--- Udemy_bot.py
+++ Udemy_bot.py
@@ -115,10 +115,9 @@ def add_links(soup):
     :return:
     '''
     for i in soup.find_all("div", {"class": "col-md-4 col-sm-6"}):
-        if is_valid_coupon(i):
-            if is_valid_coupon(i) and is_rate_valid(i):
-                potential_urls.append(get_udemy_link(i))
-                print('\r', len(potential_urls), 'Potential Courses Scraped', end='', flush=True)
+        if is_valid_coupon(i) and is_rate_valid(i) and url_is_new(get_udemy_link(i)):
+            potential_urls.append(get_udemy_link(i))
+            print('\r', len(potential_urls), 'Potential Courses Scraped', end='', flush=True)
 
 
 def find_potential_urls():
